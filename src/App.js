87aaaf4c83d106f0728b0eaf5412c886adbc0d06
troycaselli <swordandtrowel1689@gmail.com>
2023-07-05 16:44:26 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import {React, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  useEffect(() => {
+    axios.get('http://localhost:5050/documents')
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
